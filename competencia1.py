722c1dc19ba45f96c60875b93e675adffb4343c3
rafaelanchieta <rta@ifpi.edu.br>
2025-05-09 13:22:31 -0300

    aplicar ruídos nas palavras

--- competencia1.py
+++ competencia1.py
@@ -1,78 +1,78 @@
 import random
 import re
+import string
+from nltk import word_tokenize
 
-# Adiciona ruídos que são considerados desvios de convenções da escrita
-def adicionar_ruido_convencoes_paragrafos(paragrafos, num_palavras_ruidosas):
-   
-    def distribuir_ruidos(num_palavras_ruidosas, num_paragrafos):
-        distribuicao = [num_palavras_ruidosas // num_paragrafos] * num_paragrafos
-        for i in range(num_palavras_ruidosas % num_paragrafos):
-            distribuicao[i] += 1 
-        random.shuffle(distribuicao) 
-        return distribuicao
-    
-    distribuicao_ruidos = distribuir_ruidos(num_palavras_ruidosas, len(paragrafos))
-    
-    paragrafos_com_ruido = [adicionar_ruido_convencoes(paragrafo, distribuicao_ruidos[i]) for i, paragrafo in enumerate(paragrafos)]
-    return paragrafos_com_ruido
 
-def adicionar_ruido_convencoes(texto, num_palavras_ruidosas):
-    palavras = texto.split()
-    palavras_alteradas = set() 
-    tentativas_testadas = set()
+class Competencia1:
     
-    while len(palavras_alteradas) < min(num_palavras_ruidosas, len(palavras)):
-        if len(tentativas_testadas) == len(palavras):
-            break 
+    def __init__(self, text, porcentagem_ruidos):
+        self.text = text
+        self.palavras = word_tokenize(text, language='portuguese')
+        self.porcentagem_ruidos = porcentagem_ruidos
+        self.dicionario = {}
+
+    def remove_pontuacao(self):
+        # Remove pontuação e caracteres especiais
+        self.palavras = [palavra for palavra in self.palavras if palavra not in string.punctuation]
+
+    def indice_palavras(self):
+        # Cria um dicionário com as palavras e seus índices
+        self.dicionario = {i: palavra for i, palavra in enumerate(self.palavras)}
+    
+    def adicionar_ruido(self):
+        self.remove_pontuacao()
+        self.indice_palavras()
+        print(self.dicionario)
+    
+        num_palavras = len(self.dicionario)
+        num_palavras_ruidosas = num_palavras * self.porcentagem_ruidos // 100
+
+        indices_ruidosos = random.sample(list(self.dicionario.keys()), num_palavras_ruidosas)
+        print(indices_ruidosos)
+                                                                                                                                                                                                                                                    
+        # Adiciona ruído às palavras selecionadas
+        # palavras = [Competencia1.aplicar_ruido(dicinario[indice]) for indice in indices_ruidosos]
+        for indice in indices_ruidosos:
+            Competencia1.aplicar_ruido(self.dicionario, indice)
+        print(self.dicionario)
+
+    @staticmethod
+    def aplicar_ruido(dicionario, indice):
+        if Competencia1.tem_acento(dicionario[indice]):
+            Competencia1.remover_acentos(dicionario, indice)
+        elif Competencia1.tem_maiuscula(dicionario[indice]):
+            Competencia1.introduzir_erro_capitalizacao(dicionario, indice)
+        elif Competencia1.tem_hifen(dicionario[indice]):
+            Competencia1.lidar_com_hifen(dicionario, indice)
         
-        indice = random.choice(range(len(palavras)))
-        if indice in tentativas_testadas:
-            continue 
-        
-        tentativas_testadas.add(indice)  
-        palavra_original = palavras[indice]
-        palavra_modificada = aplicar_ruido(palavra_original)
-        
-        if palavra_modificada != palavra_original:
-            palavras[indice] = palavra_modificada
-            palavras_alteradas.add(indice)
     
-    #Mostrar palavras alteradas
-    palavras_alteradas = [palavras[i] for i in palavras_alteradas]
-    print("Palavras alteradas:", palavras_alteradas)
+    @staticmethod
+    def tem_acento(palavra):
+        return bool(re.search(r'[À-ÿ]', palavra))
+    
+    @staticmethod
+    def tem_maiuscula(palavra):
+        return any(char.isupper() for char in palavra)
+    
+    @staticmethod
+    def tem_hifen(palavra):
+        return '-' in palavra
+    
+    @staticmethod
+    def remover_acentos(dicionario, indice):
+        substituicoes_acentos = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 
+                                'â': 'a', 'ê': 'e', 'ô': 'o', 'ã': 'a', 'õ': 'o', 
+                                'ç': 'c', 'à': 'a'}
+        for acentuada, sem_acento in substituicoes_acentos.items():
+            dicionario[indice] = dicionario[indice].replace(acentuada, sem_acento).replace(acentuada.upper(), sem_acento.upper())
+        
+    @staticmethod
+    def introduzir_erro_capitalizacao(dicionario, indice):
+        for char in dicionario[indice]:
+            if char.isupper():
+                dicionario[indice] = dicionario[indice].replace(char, char.lower())
 
-    return ' '.join(palavras)
-
-def aplicar_ruido(palavra):
-    opcoes_ruido = ['acento', 'capitalizacao', 'hifen']
-    for escolha in opcoes_ruido:
-        if escolha == 'acento' and tem_acento(palavra):
-            return remover_acentos(palavra)
-        elif escolha == 'capitalizacao' and tem_maiuscula(palavra):
-            return introduzir_erro_capitalizacao(palavra)
-        elif escolha == 'hifen' and tem_hifen(palavra):
-            return lidar_com_hifen(palavra)
-    return palavra
-
-def tem_acento(palavra):
-    return bool(re.search(r'[À-ÿ]', palavra))
-
-def tem_maiuscula(palavra):
-    return any(char.isupper() for char in palavra)
-
-def tem_hifen(palavra):
-    return '-' in palavra
-
-def remover_acentos(palavra):
-    substituicoes_acentos = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 
-                             'â': 'a', 'ê': 'e', 'ô': 'o', 'ã': 'a', 'õ': 'o', 
-                             'ç': 'c', 'à': 'a'}
-    for acentuada, sem_acento in substituicoes_acentos.items():
-        palavra = palavra.replace(acentuada, sem_acento).replace(acentuada.upper(), sem_acento.upper())
-    return palavra
-
-def introduzir_erro_capitalizacao(palavra):
-    return ''.join([char.lower() if char.isupper() else char for char in palavra])
-
-def lidar_com_hifen(palavra):
-    return palavra.replace('-', ' ')
+    @staticmethod
+    def lidar_com_hifen(dicionario, indice):
+        dicionario[indice] = dicionario[indice].replace('-', ' ')
